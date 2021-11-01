0197b00568c6bcb7191f538d47680b4656d5f120
dpnguyen53 <dpnguyen53@gmail.com>
2021-11-01 21:29:56 +0700

    buoi 5

---
+++ src/app/_core/services/data.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  constructor(private http: HttpClient) {}
+
+  getListCourse(): Observable<any> {
+    const url =
+      'https://elearning0706.cybersoft.edu.vn/api/QuanLyKhoaHoc/LayDanhSachKhoaHoc?MaNhom=GP01';
+    return this.http.get(url).pipe(
+      tap(() => {}),
+      catchError((error: any) => {
+        return this.handleError(error);
+      })
+    );
+  }
+
+  handleError(error: any) {
+    switch (error.status) {
+      case 300:
+        break;
+
+      case 400:
+        break;
+
+      case 500:
+        break;
+
+      default:
+        break;
+    }
+    return throwError(error);
+  }
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //home template - localhost:4200
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/home-template/home-template.module').then(
+        (m) => m.HomeTemplateModule
+      ),
+  },
+
+  //admin template - localhost:4200/admin
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./pages/admin-template/admin-template.module').then(
+        (m) => m.AdminTemplateModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
